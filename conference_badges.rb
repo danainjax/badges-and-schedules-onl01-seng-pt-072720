54d083ea277454bcf5b1033e6ae3bde01fdd51cf
Dana Pughakoff <danainjax@gmail.com>
2020-08-01 10:21:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
  return "Hello, my name is #{name}." 
 end
 
-def batch_badge_creator(names)
-  names.each do |name| badge_maker(name)
+def batch_badge_creator(attendees)
+  attendees.each do |name| badge_maker(name)
 end
 end
 
