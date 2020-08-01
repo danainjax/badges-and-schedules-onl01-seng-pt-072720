2901d00dd5615d7900c36142108bd197344be7f6
Dana Pughakoff <danainjax@gmail.com>
2020-08-01 11:17:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,5 @@ def printer(attendees)
   #puts  results of #batch_badge_creator
   #puts results of #assign_rooms
   batch_badge_creator(attendees).each {|badge| puts badge}
-  assign_rooms(attendees).each {
+  assign_rooms(attendees).each {|rooms| puts rooms}
 end
